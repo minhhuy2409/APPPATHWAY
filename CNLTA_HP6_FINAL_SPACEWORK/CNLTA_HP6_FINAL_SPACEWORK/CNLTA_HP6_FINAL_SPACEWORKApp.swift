251e5b5ce138e3d4d5cd655a5031c52f2e7d23b3
minhhuy2409 <164725462+minhhuy2409@users.noreply.github.com>
2025-06-22 10:18:24 +0700

    Add files via upload

---
+++ CNLTA_HP6_FINAL_SPACEWORK/CNLTA_HP6_FINAL_SPACEWORK/CNLTA_HP6_FINAL_SPACEWORKApp.swift
@@ -0,0 +1,17 @@
+//
+//  CNLTA_HP6_FINAL_SPACEWORKApp.swift
+//  CNLTA_HP6_FINAL_SPACEWORK
+//
+//  Created by huy bin on 22/6/25.
+//
+
+import SwiftUI
+
+@main
+struct CNLTA_HP6_FINAL_SPACEWORKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CNLTA_HP6_FINAL_SPACEWORK/CNLTA_HP6_FINAL_SPACEWORK/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CNLTA_HP6_FINAL_SPACEWORK
+//
+//  Created by huy bin on 22/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
